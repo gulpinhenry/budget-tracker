4344629b232c46b2f09f15806d8830f4192ca140
Henry Kam <h3nrykam@gmail.com>
2021-11-17 16:53:26 -0800

    add bundle and webpack

---
+++ public/webpack.config.js
@@ -0,0 +1,54 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+  entry: {
+    app: "./index.js",
+  },
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js",
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new WebpackPwaManifest({
+      fingerprints: false,
+      name: "Budget Tracker App",
+      short_name: "Budget Tracker",
+      description:
+        "An application that allows you to track your spending and deposits online and offline.",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      "theme-color": "#ffffff",
+      start_url: "/",
+      icons: [
+        {
+          src: path.resolve("icons/icon-192x192.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("assets", "icons"),
+        },
+        {
+          src: path.resolve("icons/icon-512x512.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("assets", "icons"),
+        },
+      ],
+    }),
+  ],
+};
+
+module.exports = config;
